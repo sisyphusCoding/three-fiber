5828a6df1e148ba55e7f525ac5bb1feff2aa7b64
Anish Kanna <sautronik@gmail.com>
2022-04-05 16:00:24 +0530

    done final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React , {useRef , Suspense} from 'react'
-import {Canvas , extend , useFrame} from '@react-three/fiber'
+import {Canvas , extend , useFrame , useLoader} from '@react-three/fiber'
 import {shaderMaterial} from '@react-three/drei'
 import glsl from "babel-plugin-glsl/macro"
 import './App.css';
@@ -7,25 +7,31 @@ import * as THREE from "three"
 
 const WaveShaderMaterial = shaderMaterial (
 
-		{uTime : 0 , uColor: new THREE.Color(0.95 , 0.15 , 0.4) },
+		{uTime : 0 , uColor: new THREE.Color(0 , 0 , 0) , uTexture: new THREE.Texture()},
 
 		glsl`
 		precision mediump float;
-
-		varying vec2 vUv;
 		uniform float uTime;
+		
+		varying vec2 vUv;
+		varying float vWave;
+		
+		#pragma glslify: snoise3 = require(glsl-noise/simplex/3d)
 
-		#pragma glslify : snoise3 = require(glsl-noise/simplex/3d);
+		
+
+		
 
 		void main () {
 		vUv = uv;
 
 		vec3 pos = position;
 		float noiseFreq = 1.5;
-		float noiseAmp = 0.25;
-		vec3 noisePos = vec3(pos.x * noiseFreq + uTime , pos.y , pos.z);
-		pos.z += snoise3(noisePos) * noiseAmp
-		gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+		float noiseAmp = 0.3;
+		vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y , pos.z );
+		pos.z += snoise3(noisePos) * noiseAmp;
+		vWave = pos.z  ;
+		gl_Position = projectionMatrix * modelViewMatrix * vec4(pos , 1.0);
 
 		}
 		`,
@@ -35,9 +41,14 @@ const WaveShaderMaterial = shaderMaterial (
 		precision mediump float;
 		uniform vec3 uColor;
 		uniform float uTime;
+		uniform sampler2D uTexture;
 		varying vec2 vUv;
+		varying float vWave;
+
 		void main(){
-		gl_FragColor = vec4( sin(vUv.x + uTime)  *  uColor , 1.0);
+		float wave = vWave * 0.04;		
+		vec3 texture = texture2D(uTexture  , vUv + wave).rgb;		
+		gl_FragColor = vec4( texture , 1.0);
 		}
 `
 
@@ -47,13 +58,17 @@ extend({WaveShaderMaterial})
 
 	const Wave = () => {
 
+			const textureImage = require('./socrates.jpeg');
 			const ref = useRef();
 			useFrame(({clock}) => (ref.current.uTime = clock.getElapsedTime()));
+
+
+			const [image] = useLoader(THREE.TextureLoader , [textureImage] )
 			
 		return (
 				<mesh>
-				<planeBufferGeometry args={[0.4,0.6,20,20]}/>
-				<waveShaderMaterial ref={ref} wireframe />	
+				<planeBufferGeometry args={[1.2,.8,100,100]}/>
+				<waveShaderMaterial ref={ref} uTexture={image} />	
 				</mesh>
 
 			   )
@@ -64,7 +79,7 @@ extend({WaveShaderMaterial})
 const Scene = () => {
 
 	return (
-			<Canvas camera={{fov: 10, position: [0,0,5]}} >
+			<Canvas camera={{fov: 12, position: [0,0,5]}} >
 				<Suspense fallback={null} >	
 					<Wave />
 				</Suspense>		
@@ -76,9 +91,11 @@ const Scene = () => {
 const App = () => {
 
 	return (
-
+		<>
+			
 			<Scene/>
-
+			<h1>The Death of Socrates</h1>
+		</>
 		   ) 
 }
 
